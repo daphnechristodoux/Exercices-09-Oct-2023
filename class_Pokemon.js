9598214261e2e0c1ad23748ae5f1a5bdb526547a
Daphné Christodoux <daphne.christodoux@gmail.com>
2023-10-15 19:53:18 +0200

    Ajout de mes fichiers de travail

---
+++ class_Pokemon.js
@@ -0,0 +1,22 @@
+exo.js
+class Pokemon {
+    construction(name, attack, defense, hp, luck){
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+ 
+    }
+
+    isluck(){
+        // utiliser Match random()
+        // reduire les hp du pokemon attaques
+        // condition avec islucky
+        // afficher les hp restants + les degats infliges
+    }
+}
+// instanciation pokemon1
+// instanciation pokemon2
+
+// deroulement du combat
